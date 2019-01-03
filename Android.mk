95272b78ddff2015cf52937f5fe2e5564ac2024a
ix5 <ix5@users.noreply.github.com>
2019-01-03 21:12:07 +0100

    Use /data/vendor exclusively and drop SDK checks
    No one should be using Nougat SDK/API levels any more.
    
    Signed-off-by: ix5 <ix5@users.noreply.github.com>

--- Android.mk
+++ Android.mk
@@ -5,12 +5,9 @@ LOCAL_SRC_FILES := timekeep.c
 LOCAL_MODULE := timekeep
 LOCAL_SHARED_LIBRARIES := libcutils liblog
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS := -DANDROID_SDK_VERSION=$(PLATFORM_SDK_VERSION)
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 25),)
 LOCAL_MODULE_OWNER := sony
 LOCAL_INIT_RC_64   := vendor/etc/init/timekeep.rc
 LOCAL_PROPRIETARY_MODULE := true
-endif
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
