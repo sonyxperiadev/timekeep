262d5758b4268ee19b09cacf1901018441026283
Pavel Dubrova <pashadubrova@gmail.com>
2024-11-01 23:48:19 +0200

    timekeep: Remove PLATFORM_SDK_VERSION protection
    The project uses a separate branch for each version
    of Android, there is no need to maintain support for
    outdated versions.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- Android.mk
+++ Android.mk
@@ -5,14 +5,9 @@ LOCAL_SRC_FILES := timekeep.c
 LOCAL_MODULE := timekeep
 LOCAL_SHARED_LIBRARIES := libcutils liblog
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS := -DANDROID_SDK_VERSION=$(PLATFORM_SDK_VERSION)
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 25),)
 LOCAL_MODULE_OWNER := sony
 LOCAL_INIT_RC      := vendor/etc/init/timekeep.rc
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 28),)
 LOCAL_PROPRIETARY_MODULE := true
-endif
-endif
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -24,7 +19,5 @@ LOCAL_CERTIFICATE := platform
 LOCAL_SDK_VERSION := current
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_ENABLED := disabled
-ifneq ($(call math_gt_or_eq, $(PLATFORM_SDK_VERSION), 28),)
 LOCAL_PROPRIETARY_MODULE := true
-endif
 include $(BUILD_PACKAGE)
